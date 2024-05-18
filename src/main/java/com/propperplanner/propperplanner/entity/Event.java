c1b894087b2916ef998dd7735c4bddd7d7a9d489
Jesus Gabriel Valencia Camacho <jvalencia@correo.com>
2024-05-18 12:33:48 -0700

    Create Event Entity

---
+++ src/main/java/com/propperplanner/propperplanner/entity/Event.java
@@ -0,0 +1,33 @@
+package com.propperplanner.propperplanner.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "events")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "name")
+    private String name;
+
+    public Event(Date date, String name) {
+        this.date = date;
+        this.name = name;
+    }
+
+}
