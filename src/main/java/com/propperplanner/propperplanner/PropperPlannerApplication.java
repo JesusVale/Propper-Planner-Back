6745ef0818edd175f4f62c8a651a2c6ad39f6665
Jesus Gabriel Valencia Camacho <jvalencia@correo.com>
2024-05-18 12:33:18 -0700

    Create Spring Project

---
+++ src/main/java/com/propperplanner/propperplanner/PropperPlannerApplication.java
@@ -0,0 +1,14 @@
+package com.propperplanner.propperplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropperPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropperPlannerApplication.class, args);
+		System.out.println("App Running...");
+	}
+
+}
---
+++ src/test/java/com/propperplanner/propperplanner/PropperPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propperplanner.propperplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropperPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
